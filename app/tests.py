0c2da32749ddb6319690e86506eb8cb92bb22025
Nilay Ranjan <80938367+logicalNil@users.noreply.github.com>
2023-03-16 00:13:08 +0530

    Update tests.py

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class Test(TestCase):
+    def test(self):
+        pass
+    
+    def test2(self):
+        pass
+    
+    def test3(self):
+        pass
